73075de0b2ffba83a7c7bc02134aa0b901f965ac
Flooooooooooorian <florian.weber@neuefische.de>
2025-02-07 14:41:00 +0100

    Add Map + Set

--- src/main/java/collections/Book.java
+++ src/main/java/collections/Book.java
@@ -1,5 +1,19 @@
 package collections;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title);
+    }
 }
--- src/main/java/collections/Main.java
+++ src/main/java/collections/Main.java
@@ -1,9 +1,6 @@
 package collections;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main extends Object{
 
@@ -46,6 +43,45 @@ public class Main extends Object{
 
         for (Object name : names) {
             System.out.println(name);
+        }
+
+//        int[] names = new int[10];
+        //List<String> names = new ArrayList<>();
+        Map<String, Book> bookMap = new HashMap<>();
+
+        Book b1 = new Book();
+        bookMap.put("1", b1);
+        bookMap.put("2", new Book());
+
+        System.out.println(bookMap);
+        System.out.println(bookMap.get("1"));
+
+        bookMap.remove("1");
+        System.out.println(bookMap);
+
+        if (!bookMap.containsKey("2")) {
+            bookMap.put("2", new Book());
+        }
+
+
+        Set<Book> bookSet = new HashSet<>();
+
+        bookSet.add(b1);
+
+        System.out.println(bookSet);
+
+        bookSet.remove(b1);
+        System.out.println(bookSet);
+
+        bookSet.add(b1);
+        bookSet.add(b1);
+        bookSet.add(b1);
+        bookSet.add(b1);
+
+        System.out.println(bookSet);
+
+        for (Book book : bookSet) {
+            System.out.println(book);
         }
 
     }
